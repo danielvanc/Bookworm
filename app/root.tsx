c4a7fb84b2c5b5bb48d6ddccfcd11d7b37f8a163
Daniel Van Cuylenburg <email@danielvanc.com>
2022-01-22 15:20:05 +0000

    chore: install tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -4,13 +4,18 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import tailwindStyles from "./tailwind.css";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
+
+export function links() {
+  return [{ rel: "stylesheet", href: tailwindStyles }];
+}
 
 export default function App() {
   return (
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
