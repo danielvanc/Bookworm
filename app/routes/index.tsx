92e104d9cf1b0c3cfe546f0154d28f3088b64c21
Daniel Van Cuylenburg <email@danielvanc.com>
2022-02-09 21:58:30 +0000

    feat: integrated prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,18 @@
 import BookList from "~/components/BookLIst";
 import Page from "~/components/Page";
 import { useAuth } from "~/contexts/auth";
+import { PrismaClient } from "@prisma/client";
+import { useLoaderData } from "remix";
+
+export async function loader() {
+  const prisma = new PrismaClient();
+  const allUsers = await prisma.profile.findMany();
+  return { allUsers };
+}
 
 export default function Index() {
   const { logout } = useAuth();
+  // const { allUsers } = useLoaderData();
   return (
     <Page>
       <h1>Home</h1>
