75c4108c598d257454435e5d4edb54b45b5f5c05
Daniel Van Cuylenburg <email@danielvanc.com>
2022-03-20 15:39:10 +0000

    updated README. fixed validation bits

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,40 @@ module.exports = {
     "@remix-run/eslint-config/jest",
     "prettier",
   ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+  },
   settings: {
     jest: {
       version: 27,
     },
+  },
+  rules: {
+    "no-console": "off",
+
+    // meh...
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/sort-type-union-intersection-members": "off",
+    "jsx-a11y/media-has-caption": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "jsx-a11y/alt-text": "off", // it's not smart enough...
+    "@babel/new-cap": "off",
+    "react/jsx-filename-extension": "off",
+    "@typescript-eslint/no-namespace": "off",
+
+    // I can't figure these out:
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+
+    // enable these again someday:
+    "@typescript-eslint/no-unsafe-argument": "off",
+
+    // this one isn't smart enough for our "~/" imports
+    "import/order": "off",
+
+    // for CatchBoundaries
+    "@typescript-eslint/no-throw-literal": "off",
   },
 };
