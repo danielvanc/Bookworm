31442277a241c2f6da6357d22bbe35d89f08d412
Daniel Van Cuylenburg <email@danielvanc.com>
2022-03-25 15:31:44 +0000

    feat: fetched books for discover - latest

---
+++ app/data.ts
@@ -0,0 +1,162 @@
+export const fakeBooksData = [
+  {
+    kind: "books#volume",
+    id: "0Vv6DwAAQBAJ",
+    etag: "3UGxiIAK/Ns",
+    selfLink: "https://www.googleapis.com/books/v1/volumes/0Vv6DwAAQBAJ",
+    volumeInfo: {
+      title: "The Rust Programming Language (Covers Rust 2018)",
+      authors: ["Steve Klabnik", "Carol Nichols"],
+      publisher: "No Starch Press",
+      publishedDate: "2019-08-06",
+      description:
+        "The official book on the Rust programming language, written by the Rust development team at the Mozilla Foundation, fully updated for Rust 2018. The Rust Programming Language is the official, definitive guide to Rust, a hugely popular, community-supported programming language. This is the second edition of the improved version of the free online Rust book, so well-loved in the Rust community that it is simply referred to as \"the Book\". Programmers love Rust because it allows them to write powerful code efficiently, without the risk of crashes and errors common in languages like C and C++. This book will show readers how to use Rust's robust type system to keep programs memory-safe and speedy, and make the most of the Cargo package manager that brings the pieces of a program together. The reader will learn all about Rust's ownership rules, which lie at the heart of Rust's reliability and crash-resistant compiling. The Rust Programming Language covers everything from basic concepts like variable bindings, control flow, functions, and error handling, to more advanced topics, such as crates, generics, concurrency, and the nitty gritty of Rust's type system. With improved organization, hands-on features, and a more tutorial-oriented style, this version offers a vast improvement over the original. The second edition also provides an entirely new chapter on macros and an expanded chapter on crates, two key aspects of Rust that make it so popular. Readers will also find extra appendices on Rust development tools and Rust versions.",
+      industryIdentifiers: [
+        {
+          type: "ISBN_13",
+          identifier: "9781718500440",
+        },
+        {
+          type: "ISBN_10",
+          identifier: "1718500440",
+        },
+      ],
+      readingModes: {
+        text: false,
+        image: false,
+      },
+      pageCount: 560,
+      printType: "BOOK",
+      categories: ["Computers"],
+      averageRating: 3,
+      ratingsCount: 1,
+      maturityRating: "NOT_MATURE",
+      allowAnonLogging: false,
+      contentVersion: "preview-1.0.0",
+      panelizationSummary: {
+        containsEpubBubbles: false,
+        containsImageBubbles: false,
+      },
+      imageLinks: {
+        smallThumbnail:
+          "http://books.google.com/books/content?id=0Vv6DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        thumbnail:
+          "http://books.google.com/books/content?id=0Vv6DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      },
+      language: "en",
+      previewLink:
+        "http://books.google.co.uk/books?id=0Vv6DwAAQBAJ&printsec=frontcover&dq=%22%22&hl=&cd=1&source=gbs_api",
+      infoLink:
+        "http://books.google.co.uk/books?id=0Vv6DwAAQBAJ&dq=%22%22&hl=&source=gbs_api",
+      canonicalVolumeLink:
+        "https://books.google.com/books/about/The_Rust_Programming_Language_Covers_Rus.html?hl=&id=0Vv6DwAAQBAJ",
+    },
+    saleInfo: {
+      country: "GB",
+      saleability: "NOT_FOR_SALE",
+      isEbook: false,
+    },
+    accessInfo: {
+      country: "GB",
+      viewability: "PARTIAL",
+      embeddable: true,
+      publicDomain: false,
+      textToSpeechPermission: "ALLOWED",
+      epub: {
+        isAvailable: false,
+      },
+      pdf: {
+        isAvailable: false,
+      },
+      webReaderLink:
+        "http://play.google.com/books/reader?id=0Vv6DwAAQBAJ&hl=&printsec=frontcover&source=gbs_api",
+      accessViewStatus: "SAMPLE",
+      quoteSharingAllowed: false,
+    },
+    searchInfo: {
+      textSnippet:
+        "This is the second edition of the improved version of the free online Rust book, so well-loved in the Rust community that it is simply referred to as &quot;the Book&quot;.",
+    },
+  },
+  {
+    kind: "books#volume",
+    id: "rOzUl7TCL9QC",
+    etag: "x6SBHiOdps4",
+    selfLink: "https://www.googleapis.com/books/v1/volumes/rOzUl7TCL9QC",
+    volumeInfo: {
+      title: "The Cambridge History of the Book in Britain",
+      authors: [
+        "Former Director of the Warburg Institute and Professor Emeritus of the History of Classical Tradition J B Trapp",
+      ],
+      publisher: "Cambridge University Press",
+      publishedDate: "1998",
+      description:
+        "The history of the book from 1400 to 1557: the transition from manuscripts to printed books.",
+      industryIdentifiers: [
+        {
+          type: "ISBN_10",
+          identifier: "0521573467",
+        },
+        {
+          type: "ISBN_13",
+          identifier: "9780521573467",
+        },
+      ],
+      readingModes: {
+        text: false,
+        image: true,
+      },
+      pageCount: 832,
+      printType: "BOOK",
+      categories: ["History"],
+      averageRating: 1,
+      ratingsCount: 1,
+      maturityRating: "NOT_MATURE",
+      allowAnonLogging: false,
+      contentVersion: "0.5.5.0.preview.1",
+      panelizationSummary: {
+        containsEpubBubbles: false,
+        containsImageBubbles: false,
+      },
+      imageLinks: {
+        smallThumbnail:
+          "http://books.google.com/books/content?id=rOzUl7TCL9QC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        thumbnail:
+          "http://books.google.com/books/content?id=rOzUl7TCL9QC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+      },
+      language: "en",
+      previewLink:
+        "http://books.google.co.uk/books?id=rOzUl7TCL9QC&printsec=frontcover&dq=%22%22&hl=&cd=2&source=gbs_api",
+      infoLink:
+        "http://books.google.co.uk/books?id=rOzUl7TCL9QC&dq=%22%22&hl=&source=gbs_api",
+      canonicalVolumeLink:
+        "https://books.google.com/books/about/The_Cambridge_History_of_the_Book_in_Bri.html?hl=&id=rOzUl7TCL9QC",
+    },
+    saleInfo: {
+      country: "GB",
+      saleability: "NOT_FOR_SALE",
+      isEbook: false,
+    },
+    accessInfo: {
+      country: "GB",
+      viewability: "PARTIAL",
+      embeddable: true,
+      publicDomain: false,
+      textToSpeechPermission: "ALLOWED",
+      epub: {
+        isAvailable: false,
+      },
+      pdf: {
+        isAvailable: false,
+      },
+      webReaderLink:
+        "http://play.google.com/books/reader?id=rOzUl7TCL9QC&hl=&printsec=frontcover&source=gbs_api",
+      accessViewStatus: "SAMPLE",
+      quoteSharingAllowed: false,
+    },
+    searchInfo: {
+      textSnippet:
+        "The history of the book from 1400 to 1557: the transition from manuscripts to printed books.",
+    },
+  },
+];
--- app/routes/home/overview.tsx
+++ app/routes/home/overview.tsx
@@ -1,11 +1,78 @@
-import { Outlet } from "remix";
-import DiscoverBooks from "~/components/DiscoverBooks";
+import type { LoaderFunction } from "remix";
+import type { BookPreviewList, BooksFeed } from "remix.env";
+import { Link, useCatch, json, useLoaderData, Outlet } from "remix";
+import HorizontalScroll from "react-scroll-horizontal";
+import PreviewBook from "~/components/PreviewBook";
+
+export const loader: LoaderFunction = async () => {
+  const api = `${process.env.ALL_BOOKS_API}""&maxResults=20` || "";
+
+  try {
+    const result = await fetch(api);
+    const data: BooksFeed = await result.json();
+
+    const books = data?.items.map((book) => ({
+      id: book.id,
+      title: book.volumeInfo.title,
+      image: book.volumeInfo.imageLinks?.thumbnail || "",
+      link: book.volumeInfo.canonicalVolumeLink,
+    }));
+
+    return json(books, {
+      headers: {
+        "Cache-Control": "private, max-age=3600",
+        Vary: "Cookie",
+      },
+    });
+  } catch (error) {
+    throw new Response(
+      `Ugh oh, houston we had a problem fetching the data! Error was: ${error}`,
+      {
+        status: 500,
+      }
+    );
+  }
+};
 
 export default function Overview() {
+  const books: BookPreviewList = useLoaderData();
+
   return (
     <>
-      <DiscoverBooks />
+      <section className="flex h-[60vh] items-center overflow-x-auto bg-grayWorm-200 md:p-sectionMedium">
+        <HorizontalScroll reverseScroll={true}>
+          {/* TODO: Improve scrolling height area */}
+          <div className="flex items-center justify-center">
+            <div>
+              <h1 className="font-monty text-xl">Discover - latest!</h1>
+              <ul className="relative my-3 flex w-full gap-6">
+                {books.map((book) => (
+                  <li key={book.id} className="flex">
+                    <PreviewBook book={book} />
+                  </li>
+                ))}
+              </ul>
+              <p>
+                <Link to="/home/discover">View all</Link>
+              </p>
+            </div>
+          </div>
+        </HorizontalScroll>
+      </section>
       <Outlet />
     </>
   );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 500) {
+    return (
+      <>
+        <p>{caught.data}</p>
+        <Outlet />
+      </>
+    );
+  }
 }
