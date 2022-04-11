99afc31f2f2e1dfc544b4782460b9667716f0e49
Daniel Van Cuylenburg <email@danielvanc.com>
2022-04-11 14:02:22 +0100

    feat: preview list book item tidy up

--- app/components/PreviewListBookItem.tsx
+++ app/components/PreviewListBookItem.tsx
@@ -1,5 +1,5 @@
-import { useFetcher } from "@remix-run/react";
 import type { BookMarkItem } from "remix.env";
+import { useFetcher } from "@remix-run/react";
 import PreviewBook from "./PreviewBook";
 
 export default function PreviewListBookItem({
@@ -8,15 +8,16 @@ export default function PreviewListBookItem({
   userId,
 }: BookMarkItem) {
   const fetcher = useFetcher();
-  const errorSavingBookmark = fetcher.data?.error;
-  const itemIsBookmarked = usersBookmarks.includes(book.id);
-  const isProcessingBookmark =
+  const isBusy = fetcher.state === "submitting";
+  const hasErrors = fetcher.data?.error;
+  const isBookmarked = usersBookmarks.includes(book.id);
+  const isBookToBookmark =
     fetcher.submission?.formData.get("book_id") === book.id;
-
   const isCreatingBookmark =
+    isBookToBookmark &&
     fetcher.submission?.formData.get("bookmark") === "create";
-
   const isRemovingBookmark =
+    isBookToBookmark &&
     fetcher.submission?.formData.get("bookmark") === "delete";
 
   return (
@@ -27,29 +28,27 @@ export default function PreviewListBookItem({
         <input type="hidden" name="book_id" value={book.id} />
         <input type="hidden" name="user_id" value={userId} />
 
-        {itemIsBookmarked || isCreatingBookmark ? (
+        {isBookmarked || isCreatingBookmark ? (
           <button
             type="submit"
             name="bookmark"
-            disabled={fetcher.type === "actionSubmission"}
+            disabled={isBusy}
             value="delete"
           >
             Remove Bookmark
           </button>
-        ) : !itemIsBookmarked || (itemIsBookmarked && isRemovingBookmark) ? (
+        ) : !isBookmarked || (isBookmarked && isRemovingBookmark) ? (
           <button
             type="submit"
             name="bookmark"
-            disabled={fetcher.type === "actionSubmission"}
+            disabled={isBusy}
             value="create"
           >
             Bookmark book
           </button>
         ) : null}
 
-        {!isProcessingBookmark && errorSavingBookmark ? (
-          <p>There was an error adding this bookmark</p>
-        ) : null}
+        {hasErrors ? <p>There was an error adding this bookmark</p> : null}
       </fetcher.Form>
     </li>
   );
