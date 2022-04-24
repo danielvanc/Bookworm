9a31e7e1a384b1dbb40fb3bac51672d5f30a84cc
Daniel Van Cuylenburg <email@danielvanc.com>
2022-04-24 14:55:37 +0100

    feat: added a sprinkling of styling to preview book items

--- app/components/PreviewBookSmall.tsx
+++ app/components/PreviewBookSmall.tsx
@@ -1,18 +1,18 @@
 import { Link } from "@remix-run/react";
 
-export default function PreviewBook({ book, errors }: BookPreview) {
+export default function PreviewBookSmall({ book, errors }: BookPreview) {
   const { id, image, title } = book;
 
   return (
     <Link
       to={`/home/book/${id}`}
-      className="relative flex min-w-[350px] items-center justify-center rounded-lg bg-grayWorm-100 py-5 hover:bg-grayWorm-800"
+      className="relative my-4 flex min-w-[350px]  items-center overflow-auto rounded-xl bg-grayWorm-300/5 hover:bg-grayWorm-800"
       prefetch="intent"
     >
-      <div>
+      <div className="flex gap-3">
         {/* TODO: Provide fallback image if no image provided */}
-        <div className="max-w-[130px]">
-          <img src={image} alt={title} className="mb-3 rounded-lg" />
+        <img src={image} alt={title} className="max-h-[172px]" />
+        <div className="py-5 pr-5">
           <h3 className="font-monty">{title}</h3>
           {errors && <p className="text-red-600">{errors?.message}</p>}
         </div>
