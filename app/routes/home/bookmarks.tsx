dbff68fe67dcd2583f48e4b3946a814bf1ce8510
Daniel Van Cuylenburg <email@danielvanc.com>
2022-04-28 15:35:30 +0100

    feat(bookmarks): added initial page

--- app/routes/home/bookmarks.tsx
+++ app/routes/home/bookmarks.tsx
@@ -1,11 +1,28 @@
-export async function loader() {
-  return {};
-}
+import PreviewListBookItem from "~/components/PreviewListBookItem";
+import { useMatchesData, useUser } from "~/utils/user";
 
 export default function Bookmarks() {
+  const { id: userId } = useUser();
+  const { usersBookmarks } = useMatchesData("routes/home") as {
+    usersBookmarks: Book[];
+  };
+
   return (
     <div className="md:p-sectionMedium">
-      <h1>Bookmarks!</h1>
+      <h1 className="font-monty text-2xl md:mb-10">Your Bookmarks</h1>
+
+      <ul className="relative my-3 flex flex-wrap gap-6">
+        {usersBookmarks.map((book) => {
+          return (
+            <PreviewListBookItem
+              key={book.id}
+              book={book}
+              usersBookmarks={usersBookmarks}
+              userId={String(userId)}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 }
