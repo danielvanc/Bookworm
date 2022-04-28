731c54d9675946dff12cb10012108837a702138c
Daniel Van Cuylenburg <email@danielvanc.com>
2022-04-28 16:07:00 +0100

    feat(read): added initiial implementation

--- app/routes/home/read.tsx
+++ app/routes/home/read.tsx
@@ -1,11 +1,39 @@
-export async function loader() {
-  return {};
-}
+import type { LoaderFunction } from "@remix-run/node";
+import { oAuthStrategy, FAILURE_REDIRECT } from "~/auth/auth.server";
+import { useUser, useMatchesData } from "~/utils/user";
+import PreviewListBookItem from "~/components/PreviewListBookItem";
 
-export default function Read() {
+export const loader: LoaderFunction = async ({ request }) => {
+  await oAuthStrategy.checkSession(request, {
+    failureRedirect: FAILURE_REDIRECT,
+  });
+
+  return null;
+};
+
+export default function Reading() {
+  const { id: userId } = useUser();
+  const { usersBookmarks } = useMatchesData("routes/home") as {
+    usersBookmarks: Book[];
+  };
+  const booksReading = usersBookmarks.filter((book) => book.read);
+
   return (
     <div className="md:p-sectionMedium">
-      <h1>Read!</h1>
+      <h1 className="font-monty text-2xl md:mb-10">Read!</h1>
+
+      <ul className="relative my-3 flex flex-wrap gap-6">
+        {booksReading.map((book) => {
+          return (
+            <PreviewListBookItem
+              key={book.id}
+              book={book}
+              usersBookmarks={usersBookmarks}
+              userId={String(userId)}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 }
