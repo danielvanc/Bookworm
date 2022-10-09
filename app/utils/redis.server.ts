c9d628ba26a91fbf6e015646751ece0f95bfeafc
Daniel Van Cuylenburg <email@danielvanc.com>
2022-10-09 21:17:54 +0100

    chore: changed redis package

--- app/utils/redis.server.ts
+++ app/utils/redis.server.ts
@@ -1,33 +1,30 @@
 // TODO: Add typing
 // @ts-nocheck
-import * as redis from "redis";
+import Redis from "ioredis";
 
 // TODO: Add better caching functions
 
-const options =
+const url =
   process.env.NODE_ENV === "production"
-    ? {
-        url: process.env.REDIS_URL_PROD,
-        port: process.env.REDIS_PORT_PROD,
-        pass: process.env.REDIS_PASS_PROD,
-      }
-    : { url: process.env.REDIS_URL_LOCAL };
+    ? process.env.REDIS_URL
+    : process.env.REDIS_URL_LOCAL;
 
-const client = redis.createClient(options);
+const client = new Redis(url);
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
 export { client };
 
 export async function getCacheData(key) {
-  await client.connect();
-  const data = await client.get(key);
-  await client.quit();
+  // await client.connect();
+  const data = await client.get(key).then((result) => result);
+  // await client.quit();
   return data;
 }
 
 export const saveToRedis = async (key, data) => {
-  await client.connect();
-  await client.set(key, JSON.stringify(data), { EX: 60 * 60 });
-  await client.quit();
+  // await client.connect();
+  // await client.set(key, JSON.stringify(data), { EX: 60 * 60 });
+  await client.set(key, JSON.stringify(data), "EX", 60 * 60);
+  // await client.quit();
 };
