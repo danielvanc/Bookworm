e35ed7638fd369aa98227eab3358aedee2473b0e
Daniel Van Cuylenburg <email@danielvanc.com>
2022-11-13 17:39:10 +0000

    feat(auth): check for session on homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,21 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import {
+  FAILURE_REDIRECT,
+  getSession,
+  SUCCESS_REDIRECT,
+} from "~/auth/auth.server";
 import Faqs from "~/components/Marketing/Faq";
 import { Footer } from "~/components/Marketing/Footer";
 import Header from "~/components/Marketing/Header";
 import { SampleFeatures } from "~/components/Marketing/SampleFeatures";
+
+export async function loader({ request }: LoaderArgs) {
+  const { session } = await getSession(request);
+  if (!session) return redirect(FAILURE_REDIRECT);
+
+  return redirect(SUCCESS_REDIRECT);
+}
 
 export default function Welcome() {
   return (
